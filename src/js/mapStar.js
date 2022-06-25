16bf72ca8ade21315aeec6a27b70b98e3153ccd4
Артемчук Алексей <elusiv@mail.ru>
2022-06-25 22:57:59 +0500

    fixed lint mapStar 25.06.22

--- src/js/mapStar.js
+++ src/js/mapStar.js
@@ -1,23 +1,23 @@
-export default class Settings{
-    constructor(theme, music, difficulty) {
-      this.settings = new Map([
-        ['default', {
-            theme: 'dark', 
-            music: 'trance', 
-            difficulty: 'easy'
-        }]
-      ])
-        
-      this.settings.set('custom', {theme, music, difficulty})
+export default class Settings {
+  constructor(theme, music, difficulty) {
+    this.settings = new Map([
+      ['default', {
+        theme: 'dark',
+        music: 'trance',
+        difficulty: 'easy',
+      }],
+    ]);
+
+    this.settings.set('custom', { theme, music, difficulty });
+  }
+
+  getSettings() {
+    for (const key in this.settings.get('custom')) {
+      if (this.settings.get('custom')[key] === undefined) {
+        this.settings.get('custom')[key] = this.settings.get('default')[key];
+      }
     }
 
-    getSettings() {
-      for (let key in this.settings.get('custom')) {
-        if (this.settings.get('custom')[key]  === undefined) {
-          this.settings.get('custom')[key] = this.settings.get('default')[key]
-        }
-      }
-      
-      return this.settings.get('custom');
-    }
+    return this.settings.get('custom');
+  }
 }
